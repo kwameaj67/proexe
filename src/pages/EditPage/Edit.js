0dffd089bb3ba5e472bfcf33995b0ae41f97ad5d
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-05 18:06:07 +0000

    created editPage

---
+++ src/pages/EditPage/Edit.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './edit.css'
+
+const HomePage = () => {
+    return (
+        <div>
+            <p>EditPage</p>
+        </div>
+    )
+}
+
+export default HomePage
