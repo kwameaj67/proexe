66fe3d464920866bedbe54e8ec2de5f0eea82df1
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-05 18:04:51 +0000

    created header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+
+ const Header = () => {
+    return (
+        <div className="header_container">
+            <p>Dashboard</p>
+        </div>
+    )
+}
+
+export default Header;
