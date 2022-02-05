08e72c1dc4767f622668ae60d1d0feacffd822c5
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-05 18:06:41 +0000

    create redux store

---
+++ src/redux/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore  } from '@reduxjs/toolkit'
+import userReducer from '../slices/userSlice'
+
+
+const allReducers = {
+    users:userReducer
+}
+
+
+const reduxStore = configureStore({
+    reducer:allReducers,
+    devTools:true
+})
+
+export default reduxStore;
