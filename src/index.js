b592e2a2295d31cb139556aab3a24293d253b1d5
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-05 18:07:25 +0000

    add reduxStore to entire app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import reduxStore from './redux/store/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={reduxStore}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
