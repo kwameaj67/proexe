136f9fde9bca4873a3096e3f744de5d794a26846
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-06 00:57:39 +0000

    home changes

--- src/pages/Homepage/Home.js
+++ src/pages/Homepage/Home.js
@@ -1,24 +1,27 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import './home.css'
-import { Header, } from '../../components/index'
-import { responseData } from '../../utils/data'
-import {  useDispatch,useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Header,TableRow } from '../../components/index'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchUsersAsync } from '../../redux/slices/userSlice'
 
 const HomePage = () => {
     const dispatch = useDispatch()
-    const users = useSelector( state => state.users);
-   
-    useEffect(()=>{
+    const users = useSelector(state => state.users);
+
+    useEffect(() => {
         dispatch(fetchUsersAsync())
-    },[dispatch])
+        // console.log(users)
+    }, [dispatch])
     return (
         <div className="home_container">
             <Header />
             <div className="table_container">
                 <div className="table_header">
                     <p>User list</p>
-                    <button>Add New User</button>
+                    <Link to='/create-user' style={{"textDecoration": 'none'}}>
+                        <button>Add New User</button>
+                    </Link>
                 </div>
                 <div className="table_content">
                     <table>
@@ -37,18 +40,26 @@ const HomePage = () => {
                             {
                                 users.map((item) => {
                                     return (
-                                        <tr className="text" key={item.id}>
-                                            <td>{item.id}</td>
-                                            <td>{item.name}</td>
-                                            <td>{item.username}</td>
-                                            <td>{item.email}</td>
-                                            <td>{item.address.city}</td>
-                                            <td><button className="edit_button">Edit</button></td>
-                                            <td><button className="delete_button">Delete</button></td>
-                                        </tr>
+                                        // <tr className="text" key={item.id}>
+                                        //     <td>{item.id}</td>
+                                        //     <td>{item.name}</td>
+                                        //     <td>{item.username}</td>
+                                        //     <td>{item.email}</td>
+                                        //     <td>{item.address.city}</td>
+                                        //     <td><button className="edit_button">Edit</button></td>
+                                        //     <td><button className="delete_button">Delete</button></td>
+                                        // </tr>
+                                        // <TableRow 
+                                        //     id={item.id} 
+                                        //     name={item.name} 
+                                        //     username={item.username} 
+                                        //     email={item.email} 
+                                        //     />
+                                        <></>
                                     )
                                 })
                             }
+                           
                         </tbody>
                     </table>
                 </div>
