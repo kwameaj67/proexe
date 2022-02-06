37ca67b234a05485344e6dc74d792bbfda012538
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-06 23:34:41 +0000

    modified router loader

--- src/router/router.js
+++ src/router/router.js
@@ -1,24 +1,28 @@
 import React, { Suspense } from 'react';
-import { HomePage, EditPage } from '../pages/index'
+import { HomePage, EditUserPage, CreateUserPage } from '../pages/index'
 import { Route, Routes } from 'react-router-dom'
+import './router.css'
 
-
- const ProexeRouter = () => {
+const ProexeRouter = () => {
 
     const Loader = () => {
         return (
             <div className="loader">
-                <p>Loading...</p>
+                <div>
+                    <p>Proexe...</p>
+                </div>
+
             </div>
         )
     }
     return (
         <Suspense fallback={
-           <Loader/>
+            <Loader />
         }>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/edit:id" element={<EditPage />} />
+                <Route path="/edit/:id" element={<EditUserPage />} />
+                <Route path="/create-user" element={<CreateUserPage />} />
             </Routes>
         </Suspense>
     )
