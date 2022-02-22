33c2e6306bfbe20ce763a6bfe4cc8f6c2f5942b2
kwameaj67 <kwameagyenimboateng64@gmail.com>
2022-02-22 19:22:01 +0000

    export  backButton

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export  { default as Header } from './Header/Header'
 export  { default as TableRow } from './TableRow/TableRow'
 export  { default as Modal } from './Modal/Modal'
+export  { default as BackButton } from './BackButton/BackButton'
